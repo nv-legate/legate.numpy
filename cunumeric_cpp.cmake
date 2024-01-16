c37e7cb86c8ba76f4a7a05445b113811af6d5260
Sebastian Berg <sebastianb@nvidia.com>
2024-01-16 09:46:42 +0100

    BUG: Ship operators.inl which is used by legate-dataframe (#71)

--- cunumeric_cpp.cmake
+++ cunumeric_cpp.cmake
@@ -459,6 +459,7 @@ install(
         src/cunumeric/ndarray.h
         src/cunumeric/ndarray.inl
         src/cunumeric/operators.h
+        src/cunumeric/operators.inl
         src/cunumeric/slice.h
         src/cunumeric/typedefs.h
   DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/cunumeric/cunumeric)
